3699a6c33d344adcd86a9e80a89011c5fe286936
SohailDAD <dsohail866@gmail.com>
2024-09-29 11:38:26 -0600

    Install nodemon package

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>My name is sohail dad</h1>')
+})
+
+app.get('/about',(req,res) => {
+    res.send("<h1>This is About Page</h1>")
+})
+
+app.get('/contact',(req,res) => {
+    res.send("<h1>This is Contact Page</h1>")
 })
 
 app.listen(port, () => {
