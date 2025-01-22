626aaa26fc67abe9d66d1b0fda7fe5f54fd10b44
SohailDAD <dsohail866@gmail.com>
2025-01-22 13:14:02 +0500

    add folder of Routes in learning-folder

--- CRUD operation/userModel.js
+++ CRUD operation/userModel.js
@@ -5,10 +5,7 @@ const url = "mongodb://localhost:27017/mydatabase";
 mongoose.connect(url);
 
 const userSchema = mongoose.Schema({
-    // name : 'Sohail Dad',
-    // email : 'example@gmail.com',
-    // phone : '+923*******0'
-
+    
     name : String,
     email : String,
     phone : String
